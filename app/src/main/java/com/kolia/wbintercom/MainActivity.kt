60777a29995db31299dce16a61b064f05a9e23f2
kohayoh <n.e.makarov@gmail.com>
2026-01-30 13:25:32 +0300

    Update MainActivity.kt

--- app/src/main/java/com/kolia/wbintercom/MainActivity.kt
+++ app/src/main/java/com/kolia/wbintercom/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.kolia.wbintercom
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(android.R.layout.simple_list_item_1)
+    setContentView(R.layout.activity_main)
+
+    val et = findViewById<EditText>(R.id.etRtsp)
+    val btn = findViewById<Button>(R.id.btnPlay)
+
+    // можно сразу подставить твой поток, чтобы не вводить каждый раз:
+    // et.setText("rtsp://admin:123456@192.168.0.213:554/stream1")
+
+    btn.setOnClickListener {
+      val rtsp = et.text.toString().trim()
+      startActivity(Intent(this, PlayerActivity::class.java).putExtra("rtsp", rtsp))
+    }
   }
 }
